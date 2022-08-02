ff3bc93fde9795e72ce4f1fd5296237f96ba2587
ItDev-Angel <itdevangel@gmail.com>
2022-08-02 10:26:07 +0300

    first commit

---
+++ src/components/SignUpInfo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SignUpInfo = () => {
+  return (
+    <div className="sign-up-container">
+      <input type="text" placeholder="Email..." />
+      <input type="text" placeholder="Password..." />
+      <input type="text" placeholder="Confirm..." />
+    </div>
+  )
+}
+
+export default SignUpInfo
